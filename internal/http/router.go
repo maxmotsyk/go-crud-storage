e3fac0727bcce507182ebb82d2b22ee371c2a628
MacBook <21mechanic21@gmail.com>
2025-12-11 15:28:08 +0200

    add cors

--- internal/http/router.go
+++ internal/http/router.go
@@ -1,10 +1,12 @@
 package http
 
 import (
-	"github.com/go-chi/chi/v5"
-	httpSwagger "github.com/swaggo/http-swagger"
 	_ "gocrud/internal/docs" // важливо!
 	h "gocrud/internal/hendlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func SetupRoutes(r *chi.Mux, handler *h.UserHandler) {
@@ -16,4 +18,15 @@ func SetupRoutes(r *chi.Mux, handler *h.UserHandler) {
 	})
 
 	r.Get("/swagger/*", httpSwagger.WrapHandler)
+
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 }
